88899c1fc8970689fa56fa3ca502eabc9ec8e48c
Sony2k20 <cong.son.nguyen90@gmail.com>
2024-10-11 17:28:08 +0200

    Added fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,13 @@ module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
     extend: {
+      fontFamily: {
+        aileron: ["Aileron", "sans-serif"],
+        roboto: ["Roboto", "sans-serif"],
+        times: ['"Times New Roman"', "serif"],
+        brittany: ["Brittany", "serif"],
+        sans: ["Roboto", "sans-serif"],
+      },
       colors: {
         primary: {
           light: "#DED3E3",
@@ -20,16 +27,13 @@ module.exports = {
           dark: "#3c382f",
         },
         accent: {
-          light: "",
-          DEFAULT: "",
-          dark: "",
+          light: "#8277a3",
+          DEFAULT: "#8277a3",
+          dark: "#8277a3",
         },
       },
     },
   },
-  //ToDO fontFamily
-  // fontFamily: {
-  // },
   plugins: [
     require("@tailwindcss/aspect-ratio"),
     require("@tailwindcss/forms"),
