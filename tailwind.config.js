b034f86d271485ee7a1eb5a3f988cd1b0fd49715
Sony2k20 <cong.son.nguyen90@gmail.com>
2024-10-11 11:55:42 +0200

    Added color config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,34 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          light: "#DED3E3",
+          DEFAULT: "#DED3E3",
+          dark: "#DED3E3",
+        },
+        secondary: {
+          light: "#f4f0f7",
+          DEFAULT: "#f4f0f7",
+          dark: "#f4f0f7",
+        },
+        black: {
+          light: "#3c382f",
+          DEFAULT: "#3c382f",
+          dark: "#3c382f",
+        },
+        accent: {
+          light: "",
+          DEFAULT: "",
+          dark: "",
+        },
+      },
+    },
   },
+  //ToDO fontFamily
+  // fontFamily: {
+  // },
   plugins: [
     require("@tailwindcss/aspect-ratio"),
     require("@tailwindcss/forms"),
