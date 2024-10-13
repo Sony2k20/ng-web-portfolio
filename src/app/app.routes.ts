ff8342d639d4b16af7bda80f16bb74e09a0ef7a9
Sony2k20 <cong.son.nguyen90@gmail.com>
2024-10-13 17:58:09 +0200

    Fix routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,12 @@
 import { Routes } from '@angular/router';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { PodcastComponent } from './podcast/podcast.component';
 
 export const routes: Routes = [
   { path: '', component: LandingPageComponent, pathMatch: 'full' },
   {
     path: 'podcast',
-    loadComponent: () =>
-      import('./podcast/podcast.component').then((mod) => mod.PodcastComponent),
+    component: PodcastComponent,
   },
   { path: '**', redirectTo: '', pathMatch: 'full' },
 ];
