b1e513d0d3ba53ff42a97c184c3b01767f006b38
Sony2k20 <cong.son.nguyen90@gmail.com>
2024-10-13 18:01:06 +0200

    Revert routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,12 @@
 import { Routes } from '@angular/router';
 import { LandingPageComponent } from './landing-page/landing-page.component';
-import { PodcastComponent } from './podcast/podcast.component';
 
 export const routes: Routes = [
   { path: '', component: LandingPageComponent, pathMatch: 'full' },
   {
     path: 'podcast',
-    component: PodcastComponent,
+    loadComponent: () =>
+      import('./podcast/podcast.component').then((mod) => mod.PodcastComponent),
   },
   { path: '**', redirectTo: '', pathMatch: 'full' },
 ];
