7210ed56e5965306b4428ddb3d44fce932d97aee
Sony2k20 <cong.son.nguyen90@gmail.com>
2024-10-20 17:54:33 +0200

    Wip about me

---
+++ src/app/landing-page/coaching/coaching.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-coaching',
+  standalone: true,
+  imports: [],
+  templateUrl: './coaching.component.html',
+})
+export class CoachingComponent {}
--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -4,6 +4,8 @@ import { HeaderComponent } from '../header/header.component';
 import { HeroSectionComponent } from './hero-section/hero-section.component';
 import { FaqSectionComponent } from './faq-section/faq-section.component';
 import { FooterComponent } from '../footer/footer.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { CoachingComponent } from './coaching/coaching.component';
 
 @Component({
   selector: 'app-landing-page',
@@ -14,6 +16,8 @@ import { FooterComponent } from '../footer/footer.component';
     HeroSectionComponent,
     FaqSectionComponent,
     FooterComponent,
+    AboutMeComponent,
+    CoachingComponent,
   ],
   templateUrl: './landing-page.component.html',
 })
