cf08d960a6bf7823b5e472f0af599cf259d66e79
Sony2k20 <cong.son.nguyen90@gmail.com>
2024-11-02 22:09:17 +0100

    wip mobile view

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,11 @@
 import { Component, inject } from '@angular/core';
 import { ViewportScroller } from '@angular/common';
+import { HamburgerMenuComponent } from '../shared/components/hamburger-menu/hamburger-menu.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [HamburgerMenuComponent],
   templateUrl: './header.component.html',
 })
 export class HeaderComponent {
