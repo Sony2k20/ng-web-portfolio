aed4c126090125e5e225e3b657f74231e960f805
Sony2k20 <cong.son.nguyen90@gmail.com>
2024-11-24 21:48:48 +0100

    Improved assets size

--- src/app/footer/imprint/imprint.component.ts
+++ src/app/footer/imprint/imprint.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  AfterViewInit,
+  ElementRef,
+  Renderer2,
+} from '@angular/core';
 import { gsap } from 'gsap';
 import DrawSVGPlugin from 'gsap/DrawSVGPlugin';
 import { HeaderComponent } from '../../header/header.component';
@@ -11,4 +17,39 @@ import { FooterComponent } from '../footer.component';
   templateUrl: './imprint.component.html',
   styleUrls: ['./test.css'],
 })
-export class ImprintComponent {}
+export class ImprintComponent implements AfterViewInit {
+  private masks: string[] = [
+    'M',
+    'a',
+    'r',
+    'k-1',
+    'k-2',
+    'e',
+    't-line-v',
+    't-line-h',
+    'i-2',
+    'i-dot',
+    'n',
+    'g',
+    'lab-l',
+    'lab-a',
+    'lab-b',
+  ];
+
+  constructor(
+    private renderer: Renderer2,
+    private el: ElementRef,
+  ) {}
+
+  ngAfterViewInit(): void {
+    this.masks.forEach((mask) => {
+      const id = `#mask-${mask}`;
+      const path = this.el.nativeElement.querySelector(id) as SVGPathElement;
+      if (path) {
+        const length = path.getTotalLength();
+        this.renderer.setStyle(path, 'strokeDasharray', length);
+        this.renderer.setStyle(path, 'strokeDashoffset', length);
+      }
+    });
+  }
+}
