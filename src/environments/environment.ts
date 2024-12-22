70fdc700adf96882702048e3009ad4e1890b4318
Sony2k <cong.son.nguyen90@gmail.com>
2024-12-23 00:17:09 +0100

    Updated action

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   emailServerUrl:
-    process.env['EMAIL_SERVER_URL'] || 'http://localhost:3000/email',
+    process.env['EMAIL_SERVER_URL'] || 'http://localhost:3000/email/send',
   emailAdress: process.env['EMAIL_ADRESS'] || 'king_cong_son_nguyen@web.de',
 };
