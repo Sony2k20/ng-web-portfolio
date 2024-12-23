c4cf4b255e59cc70eaf7443a1b62a87cfabbc20b
Sony2k <cong.son.nguyen90@gmail.com>
2024-12-23 11:31:20 +0100

    Fixed api

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,5 @@
 export const environment = {
   emailServerUrl:
-    process.env['EMAIL_SERVER_URL'] ||
-    'http://nestjs-email-service:3000/email/send',
+    process.env['EMAIL_SERVER_URL'] || 'https://email.csnguyen.de/email/send',
   emailAdress: process.env['EMAIL_ADRESS'] || 'king_cong_son_nguyen@web.de',
 };
