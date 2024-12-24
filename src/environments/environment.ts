cff0c6be7ec0964da60c32ea0a55e883150f897b
Sony2k <cong.son.nguyen90@gmail.com>
2024-12-24 13:58:35 +0100

    Updated api

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   emailServerUrl:
-    process.env['EMAIL_SERVER_URL'] || 'https://email.csnguyen.de/email/send',
+    process.env['EMAIL_SERVER_URL'] || 'https://katy.csnguyen.de/api',
   emailAdress: process.env['EMAIL_ADRESS'] || 'king_cong_son_nguyen@web.de',
 };
