a389639a6cf80ad762d872904068140b2eccb003
Sony2k <cong.son.nguyen90@gmail.com>
2024-12-24 14:21:41 +0100

    Updated api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   emailServerUrl: 'https://katharinaniesche.de/api/email/send',
-  emailAdress: 'king_cong_son_nguyen@web.de',
+  emailAdress: 'coaching@katharinaniesche.de',
 };
