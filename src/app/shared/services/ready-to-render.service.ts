60864b953cbedad944ab404a4e732ef780d21500
Sony2k20 <cong.son.nguyen90@gmail.com>
2025-01-07 11:14:23 +0100

    updated loading dots

--- src/app/shared/services/ready-to-render.service.ts
+++ src/app/shared/services/ready-to-render.service.ts
@@ -17,7 +17,7 @@ export class ReadyToRenderService {
     eyesomeFont
       .load()
       .then(() => {
-        this.fontRdy$.next(false);
+        this.fontRdy$.next(true);
       })
       .catch((error) => {
         console.error('Font failed to load', error);
