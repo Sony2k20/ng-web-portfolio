3f14d29df9c43a40966b14b8d09735ff1e530ab4
Sony2k20 <cong.son.nguyen90@gmail.com>
2025-01-07 17:31:14 +0100

    wip video integration

--- src/app/landing-page/podcast/video-reel/video-reel.component.ts
+++ src/app/landing-page/podcast/video-reel/video-reel.component.ts
@@ -1,17 +1,47 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-video-reel',
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './video-reel.component.html',
   styleUrl: './video-reel.component.css',
 })
 export class VideoReelComponent {
+  @ViewChild('videoElement') videoElement!: ElementRef<HTMLVideoElement>;
+  volume: number = 0.5;
   isMuted: boolean = true;
+  isPlaying = true;
 
   toggleMute(hover: boolean) {
     setTimeout(() => {
       this.isMuted = !hover;
     }, 300);
+  }
+
+  setVolume(volume: number) {
+    if (this.videoElement) {
+      this.videoElement.nativeElement.volume = volume;
+    }
+  }
+
+  togglePlayPause() {
+    if (this.videoElement) {
+      if (this.isPlaying) {
+        this.videoElement.nativeElement.pause();
+      } else {
+        this.videoElement.nativeElement.play();
+      }
+      this.isPlaying = !this.isPlaying;
+    }
+  }
+
+  onPlay() {
+    this.isPlaying = true;
+  }
+
+  onPause() {
+    this.isPlaying = false;
   }
 }
