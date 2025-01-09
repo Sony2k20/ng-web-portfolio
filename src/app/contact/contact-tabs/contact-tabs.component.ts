62c4822414d33cd75f72647e84059ad31eb0ceaf
Sony2k20 <cong.son.nguyen90@gmail.com>
2025-01-09 12:01:18 +0100

    refactored contacts

--- src/app/contact/contact-tabs/contact-tabs.component.ts
+++ src/app/contact/contact-tabs/contact-tabs.component.ts
@@ -1,31 +1,10 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
-import {
-  FormGroup,
-  FormBuilder,
-  Validators,
-  ReactiveFormsModule,
-} from '@angular/forms';
-import { forkJoin, finalize } from 'rxjs';
-import { environment } from '../../../environments/environment';
-import { SnackbarService } from '../../shared/component-library/snackbar/service/snackbar.service';
-import {
-  EmailService,
-  EmailPayload,
-} from '../../shared/services/email.service';
-import { MainButtonComponent } from '../../shared/component-library/main-button/main-button.component';
-import { LoadingDotsComponent } from '../../shared/component-library/loading-dots/loading-dots.component';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { ContactFormComponent } from '../contact-form/contact-form.component';
 
 @Component({
   selector: 'app-contact-tabs',
-  imports: [
-    CommonModule,
-    ReactiveFormsModule,
-    MainButtonComponent,
-    LoadingDotsComponent,
-    ContactFormComponent,
-  ],
+  imports: [CommonModule, ContactFormComponent],
   templateUrl: './contact-tabs.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
