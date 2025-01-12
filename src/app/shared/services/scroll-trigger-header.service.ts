4da41f58c658570164ee2181d8c3bfa1b845f5be
Sony2k <cong.son.nguyen90@gmail.com>
2025-01-12 17:25:11 +0100

    disabled lenis

--- src/app/shared/services/scroll-trigger-header.service.ts
+++ src/app/shared/services/scroll-trigger-header.service.ts
@@ -1,25 +1,39 @@
 import { Injectable } from '@angular/core';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import gsap from 'gsap';
-import Lenis from 'lenis';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ScrollTriggerHeaderService {
+  private smoothScroll() {
+    // const scroller = document.documentElement;
+    // let previousScroll = 0;
+    // let targetScroll = window.scrollY;
+    // let currentScroll = window.scrollY;
+    // const scrollEase = 0.08; // Adjust for smoother or faster easing
+    // const updateScroll = () => {
+    //   targetScroll = window.scrollY; // Track target scroll position
+    //   currentScroll += (targetScroll - currentScroll) * scrollEase;
+    //   scroller.style.transform = `translateY(-${currentScroll}px)`; // Apply smooth transform
+    //   // Prevent layout issues
+    //   if (Math.abs(targetScroll - currentScroll) > 0.5) {
+    //     requestAnimationFrame(updateScroll);
+    //   } else {
+    //     currentScroll = targetScroll;
+    //     scroller.style.transform = `translateY(-${currentScroll}px)`;
+    //   }
+    // };
+    // window.addEventListener('scroll', () => {
+    //   if (Math.abs(targetScroll - currentScroll) > 0.5) {
+    //     requestAnimationFrame(updateScroll);
+    //   }
+    // });
+  }
+
   initScrollTriggers() {
-    // Initialize Lenis
-    const lenis = new Lenis({
-      lerp: 0.1, // Adjust for smoother or faster scrolling (0 to 1)
-    });
+    this.smoothScroll(); // Initialize custom smooth scroll
 
-    function raf(time: number) {
-      lenis.raf(time); // Update Lenis each frame
-      requestAnimationFrame(raf);
-    }
-    requestAnimationFrame(raf);
-
-    // GSAP Animation
     const showAnim = gsap
       .fromTo(
         document.querySelector('#start'),
@@ -28,12 +42,7 @@ export class ScrollTriggerHeaderService {
           opacity: 1,
           paused: true,
         },
-        {
-          yPercent: 0,
-          paused: true,
-          duration: 0.2,
-          opacity: 1.0,
-        },
+        { yPercent: 0, paused: true, duration: 0.2, opacity: 1.0 },
       )
       .progress(1);
 
@@ -48,11 +57,6 @@ export class ScrollTriggerHeaderService {
           showAnim.reverse();
         }
       },
-    });
-
-    // Notify ScrollTrigger about scroll changes with Lenis
-    lenis.on('scroll', () => {
-      ScrollTrigger.update(); // Synchronize ScrollTrigger with Lenis
     });
   }
 }
