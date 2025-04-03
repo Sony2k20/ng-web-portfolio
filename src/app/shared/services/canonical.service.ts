11f89cd7104d0c3aca7382b00eb4ccc0bf77a3c5
Sony2k20 <cong.son.nguyen90@gmail.com>
2025-04-03 19:04:10 +0200

    fix dynamic canonical tag

--- src/app/shared/services/canonical.service.ts
+++ src/app/shared/services/canonical.service.ts
@@ -1,11 +1,11 @@
-import { Injectable } from '@angular/core'
+import { inject, Injectable } from '@angular/core'
 import { Meta } from '@angular/platform-browser'
 
 @Injectable({
   providedIn: 'root',
 })
 export class CanonicalService {
-  constructor(private meta: Meta) {}
+  private meta = inject(Meta)
 
   setCanonicalURL(url: string) {
     this.meta.updateTag({ rel: 'canonical', href: url })
